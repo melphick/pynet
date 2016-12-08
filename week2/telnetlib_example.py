4c6dc45888c536f52ba018e2400e65101f62ff43
melphick <melphick@gmail.com>
2016-12-08 15:00:51 -0800

    Initial commit of week2 examples

---
+++ week2/telnetlib_example.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Example script for telnetlib
+"""
+
+import telnetlib
+import time
+import socket
+import sys
+
+TELNET_PORT = 23
+TELNET_TIMEOUT = 6
+
+def send_command(remote_conn, cmd):
+    cmd = cmd.rstrip()
+    remote_conn.write(cmd + '\n')
+    time.sleep(1)
+    return remote_conn.read_very_eager()
+
+def login(remote_conn, username, password):
+    output = remote_conn.read_until("sername:", TELNET_TIMEOUT)
+    remote_conn.write(username + '\n')
+    output += remote_conn.read_until("ssword:", TELNET_TIMEOUT)
+    remote_conn.write(password + '\n')
+    time.sleep(1)
+    return output
+
+def telnet_connect(ip_addr):
+    try:
+        return telnetlib.Telnet(ip_addr, TELNET_PORT, TELNET_TIMEOUT)
+    except socket.timeout:
+        sys.exit("Connection timed-out")
+
+
+def main():
+    ip_addr = '184.105.247.70'
+    username = 'pyclass'
+    password = '88newclass'
+
+    remote_conn = telnet_connect(ip_addr)
+
+    output = login(remote_conn, username, password)
+    print  output
+
+    output = send_command(remote_conn, 'terminal length 0')
+    print  output
+
+    output = send_command(remote_conn, 'show version')
+    print  output
+
+    remote_conn.close
+
+if __name__ == "__main__":
+    main()
